10d38cca1abebd411b4d4a94f0e0464890d222ed
Richard Surjadi <richsurjadi@gmail.com>
2016-12-01 14:33:03 +1100

    Boolean

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -15,26 +15,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
-
+    
     
     
     @IBAction func ButtonPushed(_ sender: AnyObject) {
         
-   Label.text = "Hey that would be \(Double(text1.text!)! + Double(text2.text!)!) !"
+        let addition = false
+        
+        if addition {
+            Label.text = "Hey that would be \(Double(text1.text!)! + Double(text2.text!)!) !"
+        } else {
+            Label.text = "Hey that would be \(Double(text1.text!)! - Double(text2.text!)!) !"
+        }
         
     }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-     
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
